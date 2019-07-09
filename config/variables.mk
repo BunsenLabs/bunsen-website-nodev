929d282284670636ad98d4db9816ef671d97190f
Jens John <dev@2ion.de>
2019-07-09 19:56:59 +0200

    config: new release helium-5

--- config/variables.mk
+++ config/variables.mk
@@ -11,15 +11,15 @@ RELEASE_ANNOUNCEMENT_URL   = https://forums.bunsenlabs.org/viewtopic.php?id=4772
 DDL_BASE_URL               = https://ddl.bunsenlabs.org/ddl
 ISO_BASE_NAME              = bl-$(RELEASE_CODENAME)$(RELEASE_VERSION)
 
-ISO_32                     = bl-Helium-4-i386.iso
-ISO_32_TORRENTHASH				 = e47c65d6a2f8fed531826c37c228e54c3a940e1f
-ISO_32CD                   = bl-Helium-4-i386-cd.iso
-ISO_32CD_TORRENTHASH			 = e74fbd689bc3f65bcf2d2101b4e4ca9e35e39469
-ISO_64                     = bl-Helium-4-amd64.iso
-ISO_64_TORRENTHASH				 = 7fb1b254fdbdd8863d686c7fa61b3b0b671551b1
+ISO_32                     = helium-5-i386.hybrid.iso
+ISO_32_TORRENTHASH				 = 1ac3513a7f74aa5183496856509a0393420177de
+ISO_32CD                   = helium-5-cd-i386.hybrid.iso
+ISO_32CD_TORRENTHASH			 = 560e255a3753e4bc47dac997fec043038623779b
+ISO_64                     = helium-5-amd64.hybrid.iso
+ISO_64_TORRENTHASH				 = 0a25ba737c90aa0b797d5473af2f624735a116fc
 
 ISO_32_SIZE                = 1.1G
-ISO_32CD_SIZE              = 674M
+ISO_32CD_SIZE              = 678M
 ISO_64_SIZE                = 1.1G
 
 DDL_URL_32                 = $(DDL_BASE_URL)/$(ISO_32)
@@ -28,7 +28,7 @@ DDL_URL_32CD               = $(DDL_BASE_URL)/$(ISO_32CD)
 DDL_URL_64                 = $(DDL_BASE_URL)/$(ISO_64)
 DDL_URL_64M                = https://kelaino.bunsenlabs.org/ddl/$(ISO_64)
 
-SHA256SUMS = $(DDL_BASE_URL)/bl-Helium-4.sha256sums.txt
+SHA256SUMS = $(DDL_BASE_URL)/helium-5.sha256sums.txt
 SHA256SUMS_URL_32          = $(SHA256SUMS)
 SHA256SUMS_URL_32CD        = $(SHA256SUMS)
 SHA256SUMS_URL_64          = $(SHA256SUMS)
