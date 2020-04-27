14b170caa0ab225fd028374c9e8cda31ae44b459
Jens John <dev@2ion.de>
2020-04-27 23:40:12 +0200

    donations: clean up JS and update a table row label

--- src/js/donations.js
+++ src/js/donations.js
@@ -1,18 +1,19 @@
-'use strict';
+"use strict";
 
 fetch("/donations.json")
-.then((resp) => {
-  if(resp.status == 200) {
-    resp.json()
-    .then((payload) => {
-      let cc = new ProgressBar.Line("#shortfall", {
-        duration: 1000,
-        color: "#e0ffba",
-        text: { value: `${payload.short_pct*100}% of ${payload.short_year}` }
-      }).animate(payload.short_pct);
-      document.querySelector("span#d-yearly").textContent = payload.want_pa;
-      document.querySelector("span#d-reserve").textContent = payload.reserve;
-      document.querySelector("span#d-updated").textContent = payload.last_update;
-    });
-  }
-});
+  .then(resp => {
+    if(resp.status == 200) {
+      resp
+        .json()
+        .then(payload => {
+          new ProgressBar.Line("#shortfall", {
+            duration: 1000,
+            color: "#e0ffba",
+            text: { value: `Up to ${payload.short_pct*100}% of year ${payload.short_year} funded` }
+          }).animate(payload.short_pct);
+          document.querySelector("span#d-yearly").textContent = payload.want_pa;
+          document.querySelector("span#d-reserve").textContent = payload.reserve;
+          document.querySelector("span#d-updated").textContent = payload.last_update;
+        });
+    }
+  });
