3b3c27443f19459b421dfd42862539a20891c710
Jens John <dev@2ion.de>
2020-12-25 16:03:41 +0100

    Harmonizue use of double quotes over single quotes in HTML

--- libexec/postproc.d/highlight_current_pages.sh
+++ libexec/postproc.d/highlight_current_pages.sh
@@ -5,6 +5,6 @@ set -e
 linkname=${1##*/}
 linkname=${linkname%.html}
 
-sed -i "/href.\+${linkname}/s/<a/<a class='selected'/" "$1"
+sed -i "/href.\+${linkname}/s/<a/<a class=\"selected\"/" "$1"
 
 exit 0
