d3094d1834189aaaa5d9a51292ec10f19051c861
Jens John <dev@2ion.de>
2021-01-31 15:26:13 +0100

    Update site for RELEASE lithium-2.1

--- config/variables.mk
+++ config/variables.mk
@@ -11,19 +11,19 @@ RELEASE_ANNOUNCEMENT_URL   = https://forums.bunsenlabs.org/viewtopic.php?id=6861
 DDL_BASE_URL               = https://ddl.bunsenlabs.org/ddl
 ISO_BASE_NAME              = bl-$(RELEASE_CODENAME)$(RELEASE_VERSION)
 
-ISO_32CD                   = lithium-1-cd-i386.hybrid.iso
-ISO_32CD_TORRENTHASH       = 92b307a89bc3ecf1e3b90de1a8ce4d111935c3a9
-ISO_64                     = lithium-1-amd64.hybrid.iso
-ISO_64_TORRENTHASH         = 5cfc559232feece91bf04674321e54099f93f719
+ISO_32CD                   = lithium-2.1-cd-i386.hybrid.iso
+ISO_32CD_TORRENTHASH       = 555eb939652e2249b0344b75633b859c45d4d6d3
+ISO_64                     = lithium-2.1-amd64.hybrid.iso
+ISO_64_TORRENTHASH         = 06f9b2356ebb0cf7ba79e05bd9616b4ee4b95b3b
 
-ISO_32CD_SIZE              = 682M
-ISO_64_SIZE                = 1.28G
+ISO_32CD_SIZE              = 665M
+ISO_64_SIZE                = 1.24G
 
 DDL_URL_32                 = $(DDL_BASE_URL)/$(ISO_32)
 DDL_URL_32CD               = $(DDL_BASE_URL)/$(ISO_32CD)
 DDL_URL_64                 = $(DDL_BASE_URL)/$(ISO_64)
 
-SHA256SUMS = $(DDL_BASE_URL)/lithium-1.sha256sums.txt
+SHA256SUMS = $(DDL_BASE_URL)/lithium-2.1.sha256sums.txt
 SHA256SUMS_URL_32CD        = $(SHA256SUMS)
 SHA256SUMS_URL_64          = $(SHA256SUMS)
 
